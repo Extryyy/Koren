ede34952af122069bd85c0b4793fa7aea5ced115
Extryyy <seserko.jan@gmail.com>
2024-04-29 12:28:55 +0200

    Update login.inc.php

---
+++ includes/login.inc.php
@@ -0,0 +1,22 @@
+<?php
+
+
+if(isset($_POST["submit"])){
+    $username = $_POST["uid"];
+    $pwd = $_POST["uid"];
+
+    require_once 'dbh.inc.php';
+    require_once 'functions.inc.php';
+
+    if (emptyInputLogin($username, $pwd)) {
+        header("location: ../Koda/login.php?error=emptyfields");
+        exit();
+    }
+
+
+    loginUser($conn, $username, $pwd);
+
+}else{
+    header("location: ../Koda/login.php?error=emptyfields");
+    exit();
+}
